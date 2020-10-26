71f1b639a7d25ff5cd94f00fe4b48063e107ab35
Yannistan <tanis.ioannis@gmail.com>
2020-10-26 21:33:10 +0100

    update 26-10

--- citizen.sol
+++ citizen.sol
@@ -1,35 +1,44 @@
+  // SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+import "./FirstErc20.sol";
 
+contract Etat {
 
- contract Voting {
-
+FirstErc20 public token;
 constructor(address payable _addr) public{
     superAdmin = _addr;
     admin[_addr] = true;
+    address erc20Address;
+    token = FirstErc20(erc20Address);
     //admin[msg.sender] = true;
 }
 
-/* Variables d'état
+/* Variables d'état */
 struct Citizen{
      bool isAdmin;
      uint nombreToken;
+     string name = token._name;
+     string symbol = token._symbol;
      uint age;
      bool isIll; 
      bool isWorking;
      uint256 idEnt;    
-     bool isBanned; //block.timestamp de  sanction + 520 semaines 
+     bool isBanned; //block.timestamp de la sanction + 520 semaines 
      bool canVote;
      bool isAlive;
+     uint registrationDate;
  }
  
  struct Entreprise {
      uint256 id;
-     unit256 nombreEmployes ;
-     unit256 salary_amount;
+     uint256 nombreEmployes;
+     uint256 salary_amount;
      bool isValid;
      
  }
  
 uint256 counterEnt;
+uint256 counterCitizen;
  
 //mapping (address => Entreprise) public mapcompany;
 
@@ -37,11 +46,13 @@ mapping (uint => Entreprise) public enterprises;
  
 mapping (address => Citizen) public citizens;
 
+mapping (address => mapping(uint256 => bool)) public jobs;
+
 mapping (address => bool) public admins;
 
 mapping (address => uint256) public caisseimpots;
 
-mapping (address => uint256) public balances;
+mapping (address => uint256) public balances_citizen;
 
 mapping (address => uint256) public caissechomage;
 
@@ -51,7 +62,7 @@ mapping (address => uint256) public caisseDeces;
 
 mapping (address => uint256) public montantretraite;
 
-enum Peines { legere, lourde, grave, tropgrave };
+enum Peines { legere, lourde, grave, tropgrave }
 
 
 function registerEntreprise(uint256 _employes) public {
@@ -62,14 +73,15 @@ function registerEntreprise(uint256 _employes) public {
 }
 
 function registerCitizen(address _addrcitizen, unit _age, bool _isIll ) public {
+ counterCitizen++;
  citizens[_addrcitizen].isAlive = true;   
  citizens[_addrcitizen].nombreToken = 100;
  citizens[_addrcitizen].age = _age;
  citizens[_addrcitizen].isIll = _isIll;
- if (citizens[_addrcitizen].age >=18 && citizens[_addrcitizen].isIll == false) {
-  citizens[_addrcitizen].idEnt = ;
- 
-    
+ citizens[_addrcitizen].registrationDate = block.timestamp;
+ if (citizens[_addrcitizen].age >=18 && citizens[_addrcitizen].isIll == false && counterCitizen == CounterEnt) {
+  jobs[_addrcitizen][CounterEnt] = true;
+}
 }
 
 
@@ -80,11 +92,17 @@ modifier onlyAdmin (){
     
 
 function paymentsalary(address _addrcitizen) public payable {
-    uint time;
-    if (block.timestamp + 4 weeks == time) 
+   // uint time;
+    if ((block.timestamp - citizens[_addrcitizen].registrationDate) % 4 weeks == 0 ) 
     balances[_addrcitizen] +=100*10**18;
 }
 
+function votecontrol (address _addrcitizen) public {
+    if (citizens[_addrcitizen].nombreToken == 0)
+    citizens[_addrcitizen].canVote = false;
+    else 
+    citizens[_addrcitizen].canVote = true;
+}
 
 function pay() public payable {
        
